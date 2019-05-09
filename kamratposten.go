9d8b6e744d43046cdf1a686f94a38e538ad48784
Fredrik Broman <fredrik.broman@me.com>
2019-05-09 22:24:13 +0200

    Add struct for items (posts and comments)

--- kamratposten.go
+++ kamratposten.go
@@ -8,6 +8,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type item struct {
+	ID      int64
+	Title   string
+	URL     string
+	Image   string
+	Text    string
+	Created time.Time
+	Parent  int64
+	Kid     int64
+	By      string // user
+	Kind    string // post || comment
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	data := "kamratposten"
 	tmpl := template.Must(template.ParseFiles("index.html"))
