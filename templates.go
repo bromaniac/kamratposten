70ccc2061c21783e91145648ac29e82903a9be39
Fredrik Broman <fredrik.broman@me.com>
2019-05-10 13:58:00 +0200

    Add file with html templates

---
+++ templates.go
@@ -0,0 +1,29 @@
+package main
+
+func index() string {
+	var index = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>{{.}}</title>
+	</head>
+	<body>
+    </body>
+</html>`
+	return index
+}
+
+func post() string {
+	var post = ``
+	return post
+}
+
+func login() string {
+	var login = ``
+	return login
+}
+
+func submitForm() string {
+	var submitForm = ``
+	return submitForm
+}
