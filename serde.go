313dbad8f5a1310a9b58a877e03fda854d888d89
Fredrik Broman <Fredrik.Broman@me.com>
2019-10-29 16:24:35 +0100

    Create data file with threads

--- serde.go
+++ serde.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 )
 
 func readItem(n int64) item {
-	file, err := ioutil.ReadFile("items/" + strconv.FormatInt(n, 10) + ".json")
+	file, err := ioutil.ReadFile("items/" + strconv.FormatInt(n, 10))
 	check(err)
 
 	data := item{}
@@ -19,9 +21,19 @@ func readItem(n int64) item {
 }
 
 func writeItem(i item, n int64) {
+	i.ID = n
+
+	if i.Parent == 0 {
+		f, err := os.OpenFile("threads.dat", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		check(err)
+		defer f.Close()
+		f.WriteString(fmt.Sprintf("%d\n", n))
+		i.Kind = "thread"
+	}
+
 	b, err := json.Marshal(i)
 	check(err)
 
-	err = ioutil.WriteFile("items/" + strconv.FormatInt(n, 10)+".json", b, 0644)
+	err = ioutil.WriteFile("items/"+strconv.FormatInt(n, 10), b, 0644)
 	check(err)
 }
